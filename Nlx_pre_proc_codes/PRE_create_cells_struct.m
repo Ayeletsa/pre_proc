678465a3885fade5df6791fe6565df46dad298d9
ayeletsa <ayeletsarel@gmail.com>
2019-06-05 08:25:45 +0300

    changed the loding of the files because now with plexon we save the all units in the same file after sorting

--- Nlx_pre_proc_codes/PRE_create_cells_struct.m
+++ Nlx_pre_proc_codes/PRE_create_cells_struct.m
@@ -13,11 +13,18 @@ for ii_cell = 1:length(C)
     p = load (file_name);
     p = p.p;
     
+    main_dir = p.path_day_dir;
+    date = char (regexp (main_dir,'\d{8}','match'));
     data_out_dir = p.path_dataout;
-    
+    main_dir_out = [p.path_dataout,'\',p.year_bat_path,'\',date];
+    dir_sorting = fullfile(main_dir_out,'\spike_sorting\');
+
     %% 1. load spike file:
-    filename_spike = fullfile(data_out_dir,P(1).year_bat_path,...
-        num2str(C(ii_cell).day),'detected_spikes',['spikes__TT',num2str(C(ii_cell).TT),'_SS_0',num2str(C(ii_cell).cell_id),'.ntt']);
+    %% Get files
+    TT=C(ii_cell).TT;
+    spike_TT_folder = dir( fullfile(dir_sorting,['*_TT',num2str(TT),'*']) );
+    spike_TT_file=fullfile(dir_sorting,spike_TT_folder.name);
+    cell_id=C.cell_id;
     
     % ==== Read the Ntt files, and extract the data that occureed within the session: ====
     FieldSelection = [1 1 1 1 1] ; % Will read ALL the parameters, including the Samples (waveforms)
@@ -25,31 +32,33 @@ for ii_cell = 1:length(C)
     ExtractionModeArray = [] ; % Will read all the data
     
     [Timestamps_usec, ChanNum, CellNumbersSpikeSorting, NumValidSamples, Samples, NlxHeader] = ...
-        Nlx2MatSpike( filename_spike, FieldSelection, 1, ExtractionMode, ExtractionModeArray ) ;
+        Nlx2MatSpike( spike_TT_file, FieldSelection, 1, ExtractionMode, ExtractionModeArray ) ;
+    %Take only relevant spikes for cell
+    Timestamps_usec=Timestamps_usec(CellNumbersSpikeSorting==cell_id);
+    Samples=Samples(:,:,CellNumbersSpikeSorting==cell_id);
     
     %% 2. fit timestamps to bsp
     
-    %a. for spikes
-    Timestamps_msec = Timestamps_usec / 1e3;
-    Timestamps_fitted_to_bsp_msec = polyval (p.time_conv_p_msec_nlx2bsp,Timestamps_msec);
-   
-    %b. for nlg events
-    start_time_msec = [p.S.start_time] / 1e3;
-    start_time_fitted_to_bsp_msec =num2cell( polyval (p.time_conv_p_msec_nlx2bsp,start_time_msec)');
-    [p.S(:).start_time_fitted_to_bsp_msec]= deal(start_time_fitted_to_bsp_msec{:});
-    
-    end_time_msec =[ p.S.end_time ]/ 1e3;
-    end_time_fitted_to_bsp_msec = num2cell(polyval (p.time_conv_p_msec_nlx2bsp,end_time_msec));
-    [p.S(:).end_time_fitted_to_bsp_msec]= deal(end_time_fitted_to_bsp_msec{:});
-    
+%     %a. for spikes
+%     Timestamps_msec = Timestamps_usec / 1e3;
+%     Timestamps_fitted_to_bsp_msec = polyval (p.time_conv_p_msec_nlx2bsp,Timestamps_msec);
+%    
+%     %b. for nlg events
+%     start_time_msec = [p.S.start_time] / 1e3;
+%     start_time_fitted_to_bsp_msec =num2cell( polyval (p.time_conv_p_msec_nlx2bsp,start_time_msec)');
+%     [p.S(:).start_time_fitted_to_bsp_msec]= deal(start_time_fitted_to_bsp_msec{:});
+%     
+%     end_time_msec =[ p.S.end_time ]/ 1e3;
+%     end_time_fitted_to_bsp_msec = num2cell(polyval (p.time_conv_p_msec_nlx2bsp,end_time_msec));
+%     [p.S(:).end_time_fitted_to_bsp_msec]= deal(end_time_fitted_to_bsp_msec{:});
+     
     
     %% 3. load TT info and compute cells separation
     
     L_Ratio = 0; Isolation_dis = 0;
-    NTT_file_all =   fullfile(data_out_dir,P(1).year_bat_path,...
-        num2str(C(ii_cell).day),'detected_spikes',['spikes__TT',num2str(C(ii_cell).TT),'.ntt']);
+  
     
-    [all_spike_ts,all_spike_samples] = Nlx2MatSpike(NTT_file_all, [1 0 0 0 1], 0, 1, []);
+    [all_spike_ts,all_spike_samples] = Nlx2MatSpike(spike_TT_file, [1 0 0 0 1], 0, 1, []);
     
     
     % Maya's way to compute cluster quality (for test)
@@ -69,21 +78,7 @@ for ii_cell = 1:length(C)
     % TODO: change names of params if changed in P
     cell_struct.cell_info=C(ii_cell);
     cell_struct.cell_info.TT_depth=p.depth(C(ii_cell).TT);
-    %TO DO - take the threshold from spike detection!
-    % cell_struct.cell_info.Spike_threshold_uV_units=p.Spike_threshold_uV_units(:,C(ii_cell).TT);
-    cell_struct.cell_info.include_negative_threshold=p.include_negative_threshold;
-    cell_struct.cell_info.compare_to_library=p.compare_to_library  ;
-    cell_struct.cell_info.do_coincidence_detection=p.do_coincidence_detection;
-    cell_struct.cell_info.r_threshold=p.r_threshold  ;
-    cell_struct.cell_info.threhold_factor=p.threhold_factor; %for voltage threshold (threhold=median*factor)
-    cell_struct.cell_info.min_sep_events=p.min_sep_events; %min separation between events - ~=500us
-    cell_struct.cell_info.coincidence_window=p.coincidence_window; % Duration of coincidence-detection window (in microsec) ACROSS tetrodes
-    cell_struct.cell_info.thresh_for_coinc=p.thresh_for_coinc; %remove if X tetrodes has event in the window
-%    if ~isempty(p.voltage_treshold_for_detection)
-%     cell_struct.cell_info.voltage_treshold_for_detection=p.voltage_treshold_for_detection(:,C(ii_cell).TT);
-%    end
-    cell_struct.cell_info.reference_channel=p.reference_channel;
-    cell_struct.cell_info.time_conv_p_msec_nlx2bsp=p.time_conv_p_msec_nlx2bsp;
+     
     %b. experiment information:
     %--------------------------------------------------------------
     cell_struct.exp_info.bat=p.bat;
@@ -93,9 +88,10 @@ for ii_cell = 1:length(C)
     cell_struct.exp_info.path_dataout=p.path_dataout;
     cell_struct.exp_info.bsp_tag_self=p.bsp_tag_self;
     cell_struct.exp_info.bsp_tag_other=p.bsp_tag_other;
-    %cell_struct.exp_info.landmarks = p.landmarks;
+    cell_struct.exp_info.landmarks = p.landmarks;
     cell_struct.exp_info.throw_away_times=p.throw_away_times;
     cell_struct.exp_info.nlg_events=p.S;
+    
     %c. bsp data
     %--------------------------------------------------------------
     cell_struct.bsp_data=p.bsp_data;
@@ -103,8 +99,6 @@ for ii_cell = 1:length(C)
     %d.spikes:
     %--------------------------------------------------------------
     cell_struct.spikes.spikes_ts_usec=Timestamps_usec;
-    cell_struct.spikes.spikes_ts_msec=Timestamps_msec;
-    cell_struct.spikes.spikes_ts_fitted_to_bsp_msec=Timestamps_fitted_to_bsp_msec;
     cell_struct.spikes.Samples=Samples;
     cell_struct.spikes.L_Ratio=L_Ratio;
     cell_struct.spikes.Isolation_dis=Isolation_dis;
@@ -128,7 +122,7 @@ for ii_cell = 1:length(C)
     
     %5. Save cell struct
     %TO DO - change to proc_data dir...
-    stuct_file_name=fullfile(data_out_dir,P(1).year_bat_path,'cell_structs',['bat_',num2str(p.bat),'_day_',num2str(p.day),'_cell_',num2str(cell_struct.cell_info.cell_num),'.mat']);
+    stuct_file_name=fullfile(P.path_dataout,P.year_bat_path  ,'cell_structs',['bat_',num2str(p.bat),'_day_',num2str(p.day),'_cell_',num2str(cell_struct.cell_info.cell_num),'.mat']);
     
     save(stuct_file_name,'cell_struct')
     
