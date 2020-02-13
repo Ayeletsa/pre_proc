6ea54c9f34f8e55bdb3e87241e8c047b32381ee5
ayeletsa <ayeletsarel@gmail.com>
2020-02-13 14:12:35 +0200

    changed to original unit

--- new_codes/sync_code/initial_extract_nlg_TTL.m
+++ new_codes/sync_code/initial_extract_nlg_TTL.m
@@ -1,10 +1,9 @@
-function nlg_TTL_ts_msec=initial_extract_nlg_TTL(nlx_dir)
-
+function nlg_TTL_ts_us=initial_extract_nlg_TTL(nlx_dir)
 
 nlg_TTL_file_name = fullfile(nlx_dir, 'EVENTS__Digital in.nev');
 FieldSelection = [1 0 0 0 0];
 ExtractHeader = 0;
 ExtractMode = 1;
 ModeArray = [];
-nlg_TTL_ts_usec = Nlx2MatEV( nlg_TTL_file_name ,FieldSelection,ExtractHeader,ExtractMode,ModeArray);
-nlg_TTL_ts_msec = nlg_TTL_ts_usec*1e-3;
+nlg_TTL_ts_us = Nlx2MatEV( nlg_TTL_file_name ,FieldSelection,ExtractHeader,ExtractMode,ModeArray);
+end
