354857b5597ca44a55d3ffd14d024ddff046b55f
ayeletsa <ayeletsarel@gmail.com>
2020-02-13 14:13:54 +0200

    convert to generic and correct bug

--- new_codes/sync_code/PRE_sync_x_2_y.m
+++ new_codes/sync_code/PRE_sync_x_2_y.m
@@ -1,9 +1,20 @@
-function sync_ts=PRE_sync_x_2_y(TTL_x,TTL_y,x_name,y_name,sync_name,sync_dir)
-%
+function sync_ts=PRE_sync_x_2_y(TTL_x_original,TTL_y_original,x_name,y_name,sync_name,sync_dir)
+%rewrote the beginning of the sync function, so it would be as generic as
+%possible with almost no assumptions on the data.
+
+rec_length_x = (TTL_x_original(end) - TTL_x_original(1));
+rec_length_y = (TTL_y_original(end) - TTL_y_original(1));
+
+%get the units of x,y: reasonable recording length + reasonable units (s/ms/us/ns/...)
+unit_x = 10^-(round(floor(log10(rec_length_x / 3600)) / 3)*3);
+unit_y = 10^-(round(floor(log10(rec_length_y / 3600)) / 3)*3);
+
+%convert to msecs
+TTL_x = TTL_x_original * unit_x * 1e3;
+TTL_y = TTL_y_original * unit_y * 1e3;
 
 X_TTL_intervals = diff(TTL_x);
 X_TTL_intervals_inc = diff(X_TTL_intervals);
-
 
 Y_TTL_intervals = diff(TTL_y);
 Y_TTL_intervals_inc = diff(Y_TTL_intervals);
@@ -12,16 +23,16 @@ Y_TTL_intervals_inc = diff(Y_TTL_intervals);
 thr = 2;
 x = diff(TTL_x);
 y = diff(TTL_y);
-[dist,ix,iy] = dtw(x,y);
+[~,ix,iy] = dtw(x,y);
 pairs = [x(ix);y(iy)];
 rsdl = diff(pairs);
 IX = find( abs(rsdl) < thr  );
 
 pairs_good = pairs(:,IX);
-pairs_good_x_ts = mean(TTL_x([ix(IX), ix(IX)+1]) .* 1e6,2);
+pairs_good_x_ts = mean(TTL_x([ix(IX), ix(IX)+1]),2);
 
-mathing_TTL_X_ts = TTL_x(union(ix(IX), ix(IX)+1))*1e6;
-mathing_TTL_Y_ts = TTL_y(union(iy(IX), iy(IX)+1))*1e3;
+matching_TTL_X = TTL_x(union(ix(IX), ix(IX)+1));
+matching_TTL_Y = TTL_y(union(iy(IX), iy(IX)+1));
 
 if length(unique(pairs(1,IX))) ~= length(IX) || ...
    length(unique(pairs(2,IX))) ~= length(IX)
@@ -35,7 +46,7 @@ sync_jump_ts=[];
 if ~isempty(sync_jump_ts)
     ti = [-inf sync_jump_ts inf];
     ti = [ti(1:end-1); ti(2:end)]';
-    [~, IX_per_ti] = get_data_in_ti(mathing_TTL_X_ts, ti);
+    [~, IX_per_ti] = get_data_in_ti(matching_TTL_X, ti);
 
     new_ttl_X = [];
     new_ttl_Y = [];
@@ -44,17 +55,17 @@ if ~isempty(sync_jump_ts)
         TTL_IX_after  = IX_per_ti{ii_jump+1};
         before_jump_X_ts = sync_jump_ts(ii_jump) - 1;
         after_jump_X_ts  = sync_jump_ts(ii_jump) + 1;
-        before_jump_Y_ts = interp1(mathing_TTL_X_ts(TTL_IX_before), mathing_TTL_Y_ts(TTL_IX_before), before_jump_X_ts, 'linear','extrap');
-        after_jump_Y_ts  = interp1(mathing_TTL_X_ts(TTL_IX_after ), mathing_TTL_Y_ts(TTL_IX_after ), after_jump_X_ts,  'linear','extrap');
+        before_jump_Y_ts = interp1(matching_TTL_X(TTL_IX_before), matching_TTL_Y(TTL_IX_before), before_jump_X_ts, 'linear','extrap');
+        after_jump_Y_ts  = interp1(matching_TTL_X(TTL_IX_after ), matching_TTL_Y(TTL_IX_after ), after_jump_X_ts,  'linear','extrap');
         new_ttl_X = [new_ttl_X before_jump_X_ts after_jump_X_ts];
         new_ttl_Y = [new_ttl_Y before_jump_Y_ts after_jump_Y_ts];
     end
-
-    sync_ts.X = sort([mathing_TTL_X_ts new_ttl_X]);
-    sync_ts.Y = sort([mathing_TTL_Y_ts new_ttl_Y]);
+    %save in original units!
+    sync_ts.X = sort([matching_TTL_X new_ttl_X])/unit_x*1e-3;
+    sync_ts.Y = sort([matching_TTL_Y new_ttl_Y])/unit_y*1e-3;
 else
-    sync_ts.X = mathing_TTL_X_ts;
-    sync_ts.Y = mathing_TTL_Y_ts;
+    sync_ts.X = matching_TTL_X/unit_x*1e-3;
+    sync_ts.Y = matching_TTL_Y/unit_y*1e-3;
 end
 
 %% create sync figure
@@ -65,16 +76,15 @@ pnl.pack('h',2);
 pnl(1).pack('v',2);
 pnl(2).pack('v',3);
 pnl.margin = 30;
-h=pnl.title(x_name);h.Position = [0.5 1.06]; h.FontSize=16;
+h=pnl.title(x_name);h.Position = [0.5 1.06]; h.FontSize=16;h.Interpreter='none';
 
-
-% plot TTL intervals
+%% plot TTL intervals
 pnl(1,1).select(); hold on;
-x = (TTL_x(2:end) - TTL_x(1)) .* 1e-3/60;
+x = (TTL_x(2:end) - TTL_x(1)) *1e-3/60;
 y = X_TTL_intervals*1e-3;
 plot(x,y,'.b')
 text(0.8,0.99, sprintf('n=%d',length(x)), 'Units','normalized','Color','b');
-x = (TTL_y(2:end) - TTL_y(1)) .* 1e-3/60;
+x = (TTL_y(2:end) - TTL_y(1)) *1e-3/60;
 y = Y_TTL_intervals*1e-3;
 plot(x,y,'or')
 text(0.8,0.95, sprintf('n=%d',length(x)), 'Units','normalized','Color','r');
@@ -83,12 +93,13 @@ ylabel('inter-TTL-interval (sec)')
 legend({x_name;y_name})
 title('TTL intervals timings (relative to 1st TTL)')
 
+
 pnl(1,2).select(); hold on;
-x = (TTL_x(3:end) - TTL_x(1)) .* 1e-3/60;
+x = (TTL_x(3:end) - TTL_x(1)) *1e-3/60;
 y = X_TTL_intervals_inc;
 plot(x,y,'.b')
 text(0.8,0.99, sprintf('n=%d',length(x)), 'Units','normalized','Color','b');
-x = (TTL_y(3:end) - TTL_y(1)) .* 1e-3/60;
+x = (TTL_y(3:end) - TTL_y(1)) *1e-3/60;
 y = Y_TTL_intervals_inc;
 plot(x,y,'or')
 text(0.8,0.95, sprintf('n=%d',length(x)), 'Units','normalized','Color','r');
@@ -97,27 +108,27 @@ ylabel('inter-TTL-interval increament (msec)')
 legend({x_name;y_name})
 title('TTL interval increament timings (relative to 1st TTL)')
 
-% validate resonable gain
+%% validate resonable gain
 pnl(2,1).select(); hold on;
 plot(pairs_good_x_ts, pairs_good(1,:)./pairs_good(2,:), '.-')
 plot(repmat(sync_jump_ts,2,1), repmat(get(gca,'ylim'),length(sync_jump_ts),1)', 'm-')
-rescale_plot_data(x_name, [1e-9/60 0]);
+rescale_plot_data('x', [1e-3/60 pairs_good_x_ts(1)]);
 xlabel([x_name,' time (minutes)'])
 ylabel('clock gain')
 title('only matching intervals')
 
 pnl(2,2).select(); hold on;
-plot(edges2centers(mathing_TTL_X_ts), 1e-3.*diff(mathing_TTL_X_ts) ./ diff(mathing_TTL_Y_ts),'.-')
+plot(edges2centers(matching_TTL_X),diff(matching_TTL_X) ./ diff(matching_TTL_Y),'.-')
 plot(repmat(sync_jump_ts,2,1), repmat(get(gca,'ylim'),length(sync_jump_ts),1)', 'm-')
-rescale_plot_data(x_name, [1e-9/60 0]);
+rescale_plot_data('x', [1e-3/60 pairs_good_x_ts(1)]);
 xlabel([ x_name,'time (minutes)'])
 ylabel('clock gain')
 title('all TTLs from matching intervals')
 
 pnl(2,3).select(); hold on;
-plot(edges2centers(sync_ts.X), 1e-3.*diff(sync_ts.X) ./ diff(sync_ts.Y),'.-')
+plot(edges2centers(1e-3*sync_ts.X), (unit_x.*diff(sync_ts.X)) ./ (unit_y.*diff(sync_ts.Y)),'.-')
 plot(repmat(sync_jump_ts,2,1), repmat(get(gca,'ylim'),length(sync_jump_ts),1)', 'm-')
-rescale_plot_data(x_name, [1e-9/60 0]);
+rescale_plot_data('x', [1e-3/60 pairs_good_x_ts(1)]);
 xlabel('X time (minutes)')
 ylabel('clock gain')
 title('after adding dummy TTLs in sync jumps')
